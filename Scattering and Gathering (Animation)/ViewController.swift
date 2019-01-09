f740a488fe3db084f6c02cf62b61854b879cc01e
iyinraphael <iyinraphael@gmail.com>
2019-01-09 13:39:41 -0800

    scatter function fully functional

--- Scattering and Gathering (Animation)/ViewController.swift
+++ Scattering and Gathering (Animation)/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.translatesAutoresizingMaskIntoConstraints = false
-        
         setUpLabels()
     }
     
@@ -22,7 +21,7 @@ class ViewController: UIViewController {
     
     var labels = [UILabel]()
     
-    let colors: [UIColor] = [.red, .blue, .green, .purple, .black, .yellow, .gray]
+    let colors: [UIColor] = [.red, .blue, .green, .purple, .yellow, .gray]
     
     func setUpLabels(){
         
@@ -54,11 +53,49 @@ class ViewController: UIViewController {
     }
     
     func scatter() {
-
+        
+        guard isViewLoaded else {return}
         UIView.animate(withDuration: 0.5) {
             self.imageView.alpha = 0.0
         }
         
+        for label in labels {
+            UIView.animate(withDuration: 0.5) {
+                var frame = label.frame
+                
+                let num1 = Int.random(in: 1...100)
+                let num2 = Int.random(in: 100...200)
+                
+                let randColor = Int.random(in: 0...5)
+                
+                label.transform = CGAffineTransform(rotationAngle: CGFloat(integerLiteral: num2) )
+                label.backgroundColor = self.colors[randColor]
+                
+                frame.origin.x += CGFloat(integerLiteral: num1)
+                frame.origin.y += CGFloat(integerLiteral: num2)
+                
+                label.frame = frame
+                print("\(label)")
+            }
+            
+        }
+        
+    }
+    
+    
+    func gather(){
+        
+        UIView.animate(withDuration: 0.5) {
+            self.imageView.alpha = 1.0
+        }
+        
+        var count = 0.0
+        for label in labels{
+            
+            let space: CGFloat =  (dimension * count) + (8 * count)
+            var frame = label.frame
+            frame.origin.x =
+        }
     }
 }
 
