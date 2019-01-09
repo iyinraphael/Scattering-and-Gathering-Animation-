78aba9f8aa84d491dd152547456aa26d14b180d3
iyinraphael <iyinraphael@gmail.com>
2019-01-09 12:00:38 -0800

    created labels and label's text programmatically

--- Scattering and Gathering (Animation)/ViewController.swift
+++ Scattering and Gathering (Animation)/ViewController.swift
@@ -12,6 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        setUpLabels()
+    }
+    
+    private let dimension: CGFloat = 40.0
+    
+    
+    var labels = [UILabel]()
+    
+    func setUpLabels(){
+        
+        var count: CGFloat = 1
+        
+        let lambdaString = "LAMBDA"
+        
+        for char in lambdaString {
+            
+            let space: CGFloat =  (dimension * count) + (8 * count)
+            let label = UILabel(frame: CGRect(x: space, y: 100, width: 80, height: 80))
+            view.addSubview(label)
+            label.text = "\(char)"
+            label.font = UIFont.systemFont(ofSize: 55)
+            labels.append(label)
+            count += 1
+        }
+        
         
     }
 
